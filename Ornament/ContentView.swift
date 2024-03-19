2554ec879e2a7d16494dcc09b298ba1a94120fe0
vinothvino42 <vinothvino42@gmail.com>
2024-03-19 22:12:34 +0530

    Initial Commit

---
+++ Ornament/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Ornament
+//
+//  Created by Vinoth Vino on 19/03/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Ornament/OrnamentApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrnamentApp.swift
+//  Ornament
+//
+//  Created by Vinoth Vino on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct OrnamentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

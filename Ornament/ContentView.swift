c18b6d97e367f9a7e65db5c67b3bdb37d5cd7958
vinothvino42 <vinothvino42@gmail.com>
2024-03-19 22:47:15 +0530

    Add ornaments custom views

--- Ornament/ContentView.swift
+++ Ornament/ContentView.swift
@@ -6,18 +6,79 @@
 //
 
 import SwiftUI
-import RealityKit
-import RealityKitContent
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
+        OrnamentEx1()
+//        OrnamentEx2()
+    }
+}
 
-            Text("Hello, world!")
+struct OrnamentEx1: View {
+    @State private var showHideOrnament: Visibility = .visible
+    @State private var placement: UnitPoint = .topLeading
+    @State private var anchors: [UnitPoint] = [.top, .topTrailing, .leading, .center, .trailing, .bottomLeading, .bottom, .bottomTrailing, .topLeading]
+    
+    var body: some View {
+        VStack(spacing: 26) {
+            Text("visionOS Ornament")
+            VStack(spacing: 20) {
+                Button("Show / Hide Ornament") {
+                    showHideOrnament = showHideOrnament == .visible ? .hidden : .visible
+                }
+                
+                Button("Change Placement") {
+                    let removedItem = anchors.removeFirst()
+                    placement = removedItem
+                    anchors.append(removedItem)
+                }
+            }
         }
-        .padding()
+            .ornament(visibility: showHideOrnament, attachmentAnchor: .scene(placement)) {
+                HStack(spacing: 20) {
+                    Button("Home", systemImage: "house") {
+                        
+                    }
+                    
+                    Button("Search", systemImage: "magnifyingglass") {
+                        
+                    }
+                }
+                .labelStyle(.iconOnly)
+                .padding(.all)
+                .glassBackgroundEffect()
+            }
+    }
+}
+
+struct OrnamentEx2: View {
+    var body: some View {
+        Text("visionOS Ornament")
+            .toolbar {
+                ToolbarItem(placement: .bottomOrnament) {
+                    Button("Home", systemImage: "house") {
+                        
+                    }
+                }
+                
+                ToolbarItem(placement: .bottomOrnament) {
+                    Button("Search", systemImage: "magnifyingglass") {
+                        
+                    }
+                }
+                
+                ToolbarItem(placement: .bottomOrnament) {
+                    Button("Chat", systemImage: "message") {
+                        
+                    }
+                }
+                
+                ToolbarItem(placement: .bottomOrnament) {
+                    Button("Account", systemImage: "person") {
+                        
+                    }
+                }
+            }
     }
 }
 
